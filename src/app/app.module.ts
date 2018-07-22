4d24cdb5e2c656c41ac565e7816d0629cf8ef1d8
Hrishikesh Awari <hrishiawari007@gmail.com>
2018-07-22 10:48:59 +0530

    filter with or property

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/products/filter-service.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import * as _ from "lodash";
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class FilterServiceService {
+
+  constructor() { }
+  newArray: any;
+  logicArray = [];
+  filterData(raw, filters) {
+    this.newArray = raw
+    if (filters.length > 0) {
+      filters.forEach(property => {
+        var type = property.type;
+        var value = property.value;
+        let obj = {
+          [type]: value
+        }
+        this.logicArray.push(type);
+        if (this.countInArray(this.logicArray, type) == 1) {
+          this.andOperation(obj);
+        } else {
+          this.orOperation(raw, obj);
+        }
+      });
+      this.logicArray = [];
+    } else {
+      this.newArray = raw;
+    }
+    return this.newArray;
+  }
+  andOperation(obj) {
+    this.newArray = _.filter(this.newArray, obj)
+  }
+  orOperation(raw, obj) {
+    let data = _.filter(raw, obj);
+    data.forEach(element => {
+      this.newArray.push(element);
+    });
+  }
+  countInArray(array, what) {
+    return array.filter(item => item == what).length;
+  }
+}
